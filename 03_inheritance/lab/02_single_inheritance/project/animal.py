b7c39a92f909d5d85fa27971cd41d584834560b0
Kiril Tanev <kiriltanev@proton.me>
2024-03-05 12:07:51 +0200

    02_single inheritance solution added

---
+++ 03_inheritance/lab/02_single_inheritance/project/animal.py
@@ -0,0 +1,19 @@
+class Animal:
+    """
+    Represents an animal with basic behaviors.
+
+    Attributes:
+        None specified.
+
+    Methods:
+        eat(): Simulates the animal eating, returning a string to indicate the action.
+    """
+
+    def eat(self):
+        """
+        Simulates eating action for an animal.
+
+        Returns:
+            str: A string indicating that the animal is eating.
+        """
+        return 'eating...'
---
+++ 03_inheritance/lab/02_single_inheritance/project/dog.py
@@ -0,0 +1,20 @@
+from project.animal import Animal
+
+class Dog(Animal):
+    """
+    Represents a Dog, which is a specific kind of Animal.
+
+    Inherits from Animal class to model dog-specific behavior.
+
+    Methods:
+        bark(): Simulates the dog barking, returning a string to indicate the action.
+    """
+
+    def bark(self):
+        """
+        Simulates barking action for the dog.
+
+        Returns:
+            str: A string indicating that the dog is barking.
+        """
+        return 'barking...'
