907b799918b24d840a86d90dfaff8bd9aa1ac9da
Kiril Tanev <kiriltanev@proton.me>
2024-03-05 11:40:54 +0200

    01_fruit solution added

---
+++ 03_inheritance/lab/01_food/project/food.py
@@ -0,0 +1,10 @@
+class Food:
+    """
+    A class that represents food items.
+
+    Args:
+        expiration_date (str): The expiration date of the food item.
+    """
+
+    def __init__(self, expiration_date: str):
+        self.expiration_date = expiration_date
---
+++ 03_inheritance/lab/01_food/project/fruit.py
@@ -0,0 +1,26 @@
+from project.food import Food
+
+
+class Fruit(Food):
+    """
+    A class that represents a fruit.
+
+    Parameters:
+        name (str): The name of the fruit.
+        expiration_date (str): The expiration date of the fruit.
+
+    Attributes:
+        name (str): The name of the fruit.
+        expiration_date (str): The expiration date of the fruit.
+    """
+
+    def __init__(self, name: str, expiration_date: str):
+        """
+        Initialize a Fruit object.
+
+        Args:
+            name (str): The name of the fruit.
+            expiration_date (str): The expiration date of the fruit.
+        """
+        self.name = name
+        super().__init__(expiration_date)
