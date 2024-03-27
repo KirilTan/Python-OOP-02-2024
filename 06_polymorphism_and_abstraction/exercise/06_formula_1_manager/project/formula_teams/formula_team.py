f6a5ab218e3ea8665b6f6f10e7e0de0282d8dd04
Kiril Tanev <kiriltanev@proton.me>
2024-03-27 20:54:27 +0200

    06 solution added

---
+++ 06_polymorphism_and_abstraction/exercise/06_formula_1_manager/project/formula_teams/formula_team.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+
+
+class FormulaTeam(ABC):
+    MIN_BUDGET = 1_000_000
+
+    def __init__(self, budget: int):
+        self.budget = budget
+
+    @property
+    @abstractmethod
+    def sponsors(self):
+        pass
+
+    @property
+    @abstractmethod
+    def expenses_for_one_race(self) -> int:
+        pass
+
+    @property
+    def budget(self):
+        return self.__budget
+
+    @budget.setter
+    def budget(self, value):
+        if value < FormulaTeam.MIN_BUDGET:
+            raise ValueError("F1 is an expensive sport, find more sponsors!")
+
+        self.__budget = value
+
+    def calculate_revenue_after_race(self, race_pos: int):
+        revenue: int = 0
+
+        for sponsor in self.sponsors.values():  # [{Place: Prize...}, {Place: Prize...},...]
+            for pos in sponsor:  # {Place: Prize...}
+                if race_pos <= pos:
+                    revenue += sponsor[pos]
+                    break
+
+        revenue -= self.expenses_for_one_race
+        self.budget += revenue
+
+        return f"The revenue after the race is {revenue}$. Current budget {self.budget}$"
